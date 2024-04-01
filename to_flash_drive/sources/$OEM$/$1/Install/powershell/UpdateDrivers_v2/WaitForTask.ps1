0c679c9d728592e3021263b58296a346c2452945
JustDj <fly03043090@gmail.com>
2024-04-01 10:55:46 -0400

    WaitForTask.ps1

--- to_flash_drive/sources/$OEM$/$1/Install/powershell/UpdateDrivers_v2/WaitForTask.ps1
+++ to_flash_drive/sources/$OEM$/$1/Install/powershell/UpdateDrivers_v2/WaitForTask.ps1
@@ -10,3 +10,8 @@ do {
 } until ($timer -eq $true)
 
 Restart-Computer
+
+# Make PowerShell Disappear
+$windowcode = '[DllImport("user32.dll")] public static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);'
+$asyncwindow = Add-Type -MemberDefinition $windowcode -name Win32ShowWindowAsync -namespace Win32Functions -PassThru
+$null = $asyncwindow::ShowWindowAsync((Get-Process -PID $pid).MainWindowHandle, 0)
