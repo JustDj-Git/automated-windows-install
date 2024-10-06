e3fcb6fc293cbc15c1bd1bf684bf2d269faa3bb9
JustDj <fly03043090@gmail.com>
2024-10-06 17:07:06 -0400

    Add admin request to ResetWin.bat

--- to_flash_drive/_Copy_/to_Desktop/ResetWin.bat
+++ to_flash_drive/_Copy_/to_Desktop/ResetWin.bat
@@ -1,1 +1,12 @@
+@echo off
+setlocal enabledelayedexpansion
+
+:: Admin
+net session >nul 2>&1
+if %errorlevel% neq 0 (
+    echo The script is not running as administrator! Re-run it!
+    powershell -command "Start-Process '%~f0' -Verb runAs"
+    exit /b
+)
+
 schtasks /run /tn "ResetWIN"
