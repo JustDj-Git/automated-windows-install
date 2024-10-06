3fae1c05197d566191d2d989750a5993dcaab053
JustDj <fly03043090@gmail.com>
2024-10-06 17:05:54 -0400

    Adding autoconnect to ImportWiFi.psm1

--- to_flash_drive/sources/$OEM$/$1/Install/powershell/Modules/ImportWiFi/ImportWiFi.psm1
+++ to_flash_drive/sources/$OEM$/$1/Install/powershell/Modules/ImportWiFi/ImportWiFi.psm1
@@ -3,6 +3,7 @@
     foreach ($i in $Wifi_Profiles){
     	netsh wlan delete profile "$($i.Basename)" i=* > NULL
     	netsh wlan add profile filename="$($i.FullName)" user=all > NULL
+        netsh wlan set profileparameter name="$($i.FullName)" connectionmode=auto
     	netsh wlan connect name="$($i.Basename)"
     }
 }
