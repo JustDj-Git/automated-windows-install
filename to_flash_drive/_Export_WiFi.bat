10692eda1e5355e7cef653787a510720c07f3d55
JustDj <fly03043090@gmail.com>
2024-10-07 17:33:02 -0400

    Fixed Export and Import wifi. Fixed CRLF

--- to_flash_drive/_Export_WiFi.bat
+++ to_flash_drive/_Export_WiFi.bat
@@ -12,8 +12,15 @@ if not exist "%export_dir%" (
 for /f "skip=9 tokens=1,2 delims=:" %%a in ('netsh wlan show profiles') do (
     set "profile_name=%%b"
     set "profile_name=!profile_name:~1!"
+	echo !profile_name!
 
     if not "!profile_name!"=="" (
         netsh wlan export profile name="!profile_name!" folder="%export_dir%" key=clear >nul
     )
+)
+
+for %%f in ("%export_dir%\Wi-Fi-*.xml") do (
+    set "filename=%%~nxf"
+    set "newname=!filename:Wi-Fi-=!"
+    ren "%%f" "!newname!"
 )
--- to_flash_drive/sources/$OEM$/$1/Install/powershell/Modules/ImportWiFi/ImportWiFi.psm1
+++ to_flash_drive/sources/$OEM$/$1/Install/powershell/Modules/ImportWiFi/ImportWiFi.psm1
@@ -1,9 +1,9 @@
 ﻿Function ImportWiFi {
     $Wifi_Profiles = Get-Item -Path 'C:\Install\Wifi_Profile\*.xml'
     foreach ($i in $Wifi_Profiles){
-    	netsh wlan delete profile "$($i.Basename)" i=* > NULL
-    	netsh wlan add profile filename="$($i.FullName)" user=all > NULL
-        netsh wlan set profileparameter name="$($i.FullName)" connectionmode=auto
-    	netsh wlan connect name="$($i.Basename)"
+        netsh wlan delete profile "$($i.Basename)" i=* > NULL
+        netsh wlan add profile filename="$($i.FullName)" user=all > NULL
+        netsh wlan set profileparameter name="$($i.Basename)" connectionmode=auto
+        netsh wlan connect name="$($i.Basename)"
     }
 }
