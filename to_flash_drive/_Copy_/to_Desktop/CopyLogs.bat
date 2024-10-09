ec22f56ede536653a52e0a339b69e7859e4d3fac
JustDj <fly03043090@gmail.com>
2024-10-09 19:00:12 -0400

    Moved from copy logs in LAN to upload them to OneDrive

---
+++ to_flash_drive/_Copy_/to_Desktop/CopyLogs.bat
@@ -0,0 +1,14 @@
+@echo off
+
+set "RCLONE_DIR=%SystemDrive%\Install\bats\rclone"
+set "LOG_DIR=%SystemDrive%\BIT\Files\"
+set "ONEDRIVE_REMOTE=OneDrive:BIT_Files"
+
+%RCLONE_DIR%\rclone.exe sync "%LOG_DIR%" "%ONEDRIVE_REMOTE%" --progress --config "%RCLONE_DIR%\rclone.conf"
+
+if %ERRORLEVEL% equ 0 (
+    echo Files successfully uploaded to OneDrive. >> %SystemDrive%\Install\_Logs\CopyTestLog.log
+) else (
+    echo Error occurred during file upload. >> %SystemDrive%\Install\_Logs\CopyTestLog.log
+	pause
+)
