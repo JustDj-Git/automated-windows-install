f82c6dd5bd5536f30e18e9d65c753267b91a9a67
JustDj <fly03043090@gmail.com>
2024-10-11 19:24:34 +0100

    needed CopyTestLogs.bat rename

--- to_flash_drive/_Copy_/to_Desktop/CopyTestLogs.bat
+++ to_flash_drive/_Copy_/to_Desktop/CopyTestLogs.bat
@@ -1,10 +1,10 @@
 @echo off
 
-set "RCLONE_DIR=%SystemDrive%\Install\bats\rclone"
-set "LOG_DIR=%SystemDrive%\BIT\Files\"
-set "ONEDRIVE_REMOTE=OneDrive:BIT_Files"
+set RCLONE_DIR=%SystemDrive%\Install\bats\rclone
+set LOG_DIR=%SystemDrive%\BIT\Files\
+set ONEDRIVE_REMOTE=OneDrive:BIT_Files
 
-%RCLONE_DIR%\rclone.exe sync "%LOG_DIR%" "%ONEDRIVE_REMOTE%" --progress --config "%RCLONE_DIR%\rclone.conf"
+%RCLONE_DIR%\rclone.exe sync %LOG_DIR% %ONEDRIVE_REMOTE% --progress --config %RCLONE_DIR%\rclone.conf
 
 if %ERRORLEVEL% equ 0 (
     echo Files successfully uploaded to OneDrive. >> %SystemDrive%\Install\_Logs\CopyTestLog.log
